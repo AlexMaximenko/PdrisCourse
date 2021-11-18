e0b6cc2601ff0082e4940641bddad27b8ee0610b
Maximenko Alexandr <maksimenko.ae@phystech.edu>
2021-11-19 01:22:16 +0300

    sonar error solved

--- src/main/java/sbt/Main.java
+++ src/main/java/sbt/Main.java
@@ -1,7 +1,11 @@
 package sbt;
 
+import java.util.logging.Logger;
+
 public class Main {
+
+    private static final Logger logger = Logger.getLogger(Main.class.getName());
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        logger.info("Hello World!");
     }
 }
